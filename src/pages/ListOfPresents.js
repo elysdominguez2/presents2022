aa83fe318068c884cedc325cb0205b1fb2df6524
elysdominguez <elysdominguez2@gmail.com>
2022-12-08 16:29:51 +0100

    Create the main page

---
+++ src/pages/ListOfPresents.js
@@ -0,0 +1,13 @@
+import "./pagesStyle.css";
+
+export default function ListOfPresents() {
+  return (
+    <div className="list">
+      <h1 className="title">List of presents 2022</h1>
+      <h2 className="subTitle">Presents:</h2>
+      <p className="gifts">Socks</p>
+      <p className="gifts"> Nintendo</p>
+      <p className="gifts">Lego</p>
+    </div>
+  );
+}
